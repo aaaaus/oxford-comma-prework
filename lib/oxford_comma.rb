cc0141568ca5f494a73731aad7a7fa825452df47
Austin Luft <ausluft@gmail.com>
2018-09-22 04:01:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length < 2
   array.join(", ")
   elsif array.length == 2
-  array.join(", and ")
+  array.join(" and ")
   else 
   final_element = array.pop
   array.join(", ") + ", and " + final_element
